e7b4087dcf9b8422f7e36ba2c7853fea178f7f6b
matgus217 <>
2024-01-10 12:11:17 +0000

    Add pictures to menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,22 +3,15 @@
 let index = 0;
 displayImages();
 function displayImages() {
-    let i;
-    const images = document.getElementsByClassName("image");
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
-    }
-    index++;
-    if (index > images.length) {
-        index = 1;
-    }
-    images[index - 1].style.display = "block";
-    setTimeout(displayImages, 4000);
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index - 1].style.display = "block";
+  setTimeout(displayImages, 4000);
 }
-
-
-
-
-
-
-
